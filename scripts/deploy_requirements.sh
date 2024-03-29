cf06f4a444231ec5a79a1168e3e23cc39d46cdf2
Paul Brabban <paul.brabban@gmail.com>
2024-03-29 10:28:56 +0000

    Adjust requirements for deploy with optimise plugin

--- scripts/deploy_requirements.sh
+++ scripts/deploy_requirements.sh
@@ -6,6 +6,10 @@ set -euo pipefail
 BASEDIR=$(dirname "$0")
 TEMP_REQS=${BASEDIR}/requirements.deploy.txt
 
+echo "Setting up deploy requirements.txt"
 grep -v mkdocs-material ${BASEDIR}/requirements.txt > ${TEMP_REQS}
 echo "git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git[imaging]" >> ${TEMP_REQS}
 mv ${TEMP_REQS} ${BASEDIR}/requirements.txt
+
+echo "cat ${BASEDIR}/requirements.txt..."
+cat ${BASEDIR}/requirements.txt
